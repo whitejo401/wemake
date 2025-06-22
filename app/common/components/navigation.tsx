e450c5edb9981abc4d486b08d4b340a6e2c77bab
whitjo401 <whitejo401@gmail.com>
2025-06-23 01:11:02 +0900

    3.2

--- app/common/components/navigation.tsx
+++ app/common/components/navigation.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router";
 import { Separator } from "~/common/components/ui/separator";
-import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from "./ui/navigation-menu";
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger, navigationMenuTriggerStyle } from "./ui/navigation-menu";
+import { cn } from "~/lib/utils";
 
 const menus = [
     {
@@ -111,29 +112,62 @@ const menus = [
 
 export default function Navigation() {
     return (
-        <nav className="flex px-20 h-16 items-center justify-between backdrop-blur fixed top-0 left-0 right-0 z-50 bg-background/50">
-            <div className="flex items-center gap-4">
-                <Link to="/" className="font-bold tracking-tighter text-lg">
-                    weMake
-                </Link>
-                <Separator orientation="vertical" className="h-6 mx-4" />
-                <NavigationMenu>
-                    <NavigationMenuList>
-                        {menus.map((menu) => (
-                            <NavigationMenuItem key={menu.name}>
-                                <NavigationMenuTrigger>{menu.name}</NavigationMenuTrigger>
-                                <NavigationMenuContent>
-                                   {menu.items?.map((item) => (
-                                    <NavigationMenuItem key={item.name}>                                    
-                                            <Link to={item.to}>{item.name}</Link>                                        
-                                    </NavigationMenuItem>
-                                   ))}
-                                </NavigationMenuContent>
+      <nav className="flex px-20 h-16 items-center justify-between backdrop-blur fixed top-0 left-0 right-0 z-50 bg-background/50">
+        <div className="flex items-center">
+          <Link to="/" className="font-bold tracking-tighter text-lg">
+            wemake
+          </Link>
+          <Separator orientation="vertical" className="h-6 mx-4" />
+          <NavigationMenu>
+            <NavigationMenuList>
+              {menus.map((menu) => (
+                <NavigationMenuItem key={menu.name}>
+                  {menu.items ? (
+                    <>
+                      <Link to={menu.to}>
+                        <NavigationMenuTrigger>{menu.name}</NavigationMenuTrigger>
+                      </Link>
+                      <NavigationMenuContent>
+                        <ul className="grid w-[600px] font-light gap-3 p-4 grid-cols-2">
+                          {menu.items?.map((item) => (
+                            <NavigationMenuItem
+                              key={item.name}
+                              className={cn([
+                                "select-none rounded-md transition-colors focus:bg-accent  hover:bg-accent",
+                                item.to === "/products/promote" &&
+                                  "col-span-2 bg-primary/10 hover:bg-primary/20 focus:bg-primary/20",
+                                item.to === "/jobs/submit" &&
+                                  "col-span-2 bg-primary/10 hover:bg-primary/20 focus:bg-primary/20",
+                              ])}
+                            >
+                              <NavigationMenuLink asChild>
+                                <Link
+                                  className="p-3 space-y-1 block leading-none no-underline outline-none"
+                                  to={item.to}
+                                >
+                                  <span className="text-sm font-medium leading-none">
+                                    {item.name}
+                                  </span>
+                                  <p className="text-sm leading-snug text-muted-foreground">
+                                    {item.description}
+                                  </p>
+                                </Link>
+                              </NavigationMenuLink>
                             </NavigationMenuItem>
-                        ))}                       
-                    </NavigationMenuList>
-                </NavigationMenu>
-            </div>
-        </nav>
+                          ))}
+                        </ul>
+                      </NavigationMenuContent>
+                    </>
+                  ) : (
+                    <Link className={navigationMenuTriggerStyle()} to={menu.to}>
+                      {menu.name}
+                    </Link>
+                  )}
+                </NavigationMenuItem>
+              ))}
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+      </nav>
     );
-}
+  }
