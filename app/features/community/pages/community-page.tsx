da9a5f5e2645388f2f139e5e62c8eb537031e087
whitjo401 <whitejo401@gmail.com>
2025-07-07 23:40:07 +0900

    5.8 Suspense and Await

--- app/features/community/pages/community-page.tsx
+++ app/features/community/pages/community-page.tsx
@@ -1,6 +1,6 @@
 import { Hero } from "~/common/components/hero";
 import type { Route } from "./+types/community-page";
-import { Form, Link, useSearchParams } from "react-router";
+import { Await, Form, Link, useSearchParams } from "react-router";
 import { Button } from "~/common/components/ui/button";
 import {
   DropdownMenu,
@@ -13,19 +13,24 @@ import { PERIOD_OPTIONS, SORT_OPTIONS } from "../constants";
 import { Input } from "~/common/components/ui/input";
 import { PostCard } from "../components/post-card";
 import { getPosts, getTopics } from "../queries";
+import { Suspense } from "react";
 
 export const meta: Route.MetaFunction = () => {
   return [{ title: "Community | wemake" }];
 };
 
 export const loader = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 10000));
+  // await new Promise((resolve) => setTimeout(resolve, 10000));
+  // const topics = await getTopics();
+  // const posts = await getPosts();
+  // const [topics, posts] = await Promise.all([getTopics(), getPosts()]);
   const topics = await getTopics();
-  const posts = await getPosts();
+  const posts = getPosts();
   return { topics, posts };
 };
 
 export default function CommunityPage({ loaderData }: Route.ComponentProps) {
+  const { topics, posts } = loaderData;
   const [searchParams, setSearchParams] = useSearchParams();
   const sorting = searchParams.get("sorting") || "newest";
   const period = searchParams.get("period") || "all";
@@ -99,28 +104,34 @@ export default function CommunityPage({ loaderData }: Route.ComponentProps) {
               <Link to={`/community/submit`}>Create Discussion</Link>
             </Button>
           </div>
-          <div className="space-y-5">
-            {loaderData.posts.map((post) => (
-              <PostCard
-                key={post.post_id}
-                id={post.post_id}
-                title={post.title}
-                author={post.author}
-                authorAvatarUrl={post.author_avatar}
-                category={post.topic}
-                postedAt={post.created_at}
-                votesCount={post.upvotes}
-                expanded
-              />
-            ))}
-          </div>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Await resolve={posts}>
+              {(data) => (
+                <div className="space-y-5">
+                  {data.map((post) => (
+                    <PostCard
+                      key={post.post_id}
+                      id={post.post_id}
+                      title={post.title}
+                      author={post.author}
+                      authorAvatarUrl={post.author_avatar}
+                      category={post.topic}
+                      postedAt={post.created_at}
+                      votesCount={post.upvotes}
+                      expanded
+                    />
+                  ))}
+                </div>
+              )}
+            </Await>
+          </Suspense>
         </div>
         <aside className="col-span-2 space-y-5">
           <span className="text-sm font-bold text-muted-foreground uppercase">
             Topics
           </span>
           <div className="flex flex-col gap-2 items-start">
-            {loaderData.topics.map((topic) => (
+            {topics.map((topic) => (
               <Button
                 asChild
                 variant={"link"}
--- app/features/community/queries.ts
+++ app/features/community/queries.ts
@@ -43,13 +43,14 @@ import client from "~/supa-client";
 // };
 
 export const getTopics = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 4000));
   const { data, error } = await client.from("topics").select("name, slug");
-  console.log(error);
   if (error) throw new Error(error.message);
   return data;
 };
 
 export const getPosts = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 4000));
   const { data, error } = await client
     .from("community_post_list_view")
     .select(`*`);
