ca62e930dcfffd6d4658cbe144783f454543461c
whitjo401 <whitejo401@gmail.com>
2025-07-11 23:35:04 +0900

    #6.2 Community Posts
    Add topic_slug to Database type for improved data structure

--- database.types.ts
+++ database.types.ts
@@ -735,6 +735,7 @@ export type Database = {
           post_id: number | null
           title: string | null
           topic: string | null
+          topic_slug: string | null
           upvotes: number | null
         }
         Relationships: []
