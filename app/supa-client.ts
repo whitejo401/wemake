43452fddf1ef87db00dc1d2faa92da99c2482696
whitjo401 <whitejo401@gmail.com>
2025-07-12 19:41:39 +0900

    6.8 Product Overview 기능: 제품 개요 페이지 및 쿼리 추가
    제품 개요 페이지에 로더 및 메타 함수 추가, 제품 정보를 가져오는 쿼리 구현. 데이터베이스 타입에 product_overview_view 추가 및 관련 관계 설정.

--- app/supa-client.ts
+++ app/supa-client.ts
@@ -16,6 +16,11 @@ type Database = MergeDeep<
             string | null
           >;
         };
+        product_overview_view: {
+          Row: SetNonNullable<
+            SupabaseDatabase["public"]["Views"]["product_overview_view"]["Row"]
+          >;
+        };
         gpt_ideas_view: {
           Row: SetNonNullable<
             SupabaseDatabase["public"]["Views"]["gpt_ideas_view"]["Row"]
